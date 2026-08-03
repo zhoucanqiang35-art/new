69f7a1ca18885044ee3dad7ffaf6e01bbe8ac014
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-03 15:39:35 +0800

    Deploy pikobuysheet-es site

---
+++ pikobuysheet-es/app/page.tsx
@@ -0,0 +1,5 @@
+import HomeExperience from "./home-experience";
+
+export default function Home() {
+  return <HomeExperience locale="en" />;
+}
---
+++ pikobuysheet-es/app/spreadsheet/page.tsx
@@ -0,0 +1,4 @@
+import { ContentPage } from "../content-page";
+
+export const metadata = { title: "PikoBuy Spreadsheet Research | PikoBuy Sheet", description: "Research source links, price context, QC checkpoints and shipping unknowns before moving from a PikoBuy spreadsheet row to an order." };
+export default function Page() { return <ContentPage page="spreadsheet" locale="en" />; }
