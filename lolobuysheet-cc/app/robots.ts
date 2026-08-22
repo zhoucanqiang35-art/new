e5bd0e3fe978f1f439ada918dcaec944e46e98d8
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-22 14:18:44 +0800

    Enable crawling on the production domain

--- lolobuysheet-cc/app/robots.ts
+++ lolobuysheet-cc/app/robots.ts
@@ -1,2 +1,9 @@
 import type { MetadataRoute } from "next";
-export default function robots(): MetadataRoute.Robots { return { rules: { userAgent: "*", disallow: "/" } }; }
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: { userAgent: "*", allow: "/" },
+    sitemap: "https://lolobuysheet.cc/sitemap.xml",
+    host: "https://lolobuysheet.cc",
+  };
+}
