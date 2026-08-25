8592edd1264af042591001d9705565277a0e35df
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-25 17:55:06 +0800

    Add Cloudflare Pages asset forwarding entry

---
+++ pikobuyspreadsheet-de/worker/pages-entry.js
@@ -0,0 +1,13 @@
+import app from "./server.js";
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/assets/")) {
+      return env.ASSETS.fetch(request);
+    }
+
+    return app.fetch(request, env, ctx);
+  },
+};
