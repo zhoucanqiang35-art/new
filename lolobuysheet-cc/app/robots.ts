04c6dee9f355b3392217c96c3662741b942c68db
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-25 18:08:34 +0800

    Serve robots and sitemaps as static assets

--- lolobuysheet-cc/app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    // Standard static sitemap served directly by Cloudflare Pages.
-    sitemap: "https://lolobuysheet.cc/sitemap-index.xml",
-  };
-}
