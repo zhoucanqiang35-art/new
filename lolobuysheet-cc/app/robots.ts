9d0af78cda395411db02cf39bdb221e040226c4d
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-25 16:56:35 +0800

    Advertise sitemap index

--- lolobuysheet-cc/app/robots.ts
+++ lolobuysheet-cc/app/robots.ts
@@ -4,6 +4,6 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/" },
     // Standard static sitemap served directly by Cloudflare Pages.
-    sitemap: "https://lolobuysheet.cc/sitemap.xml",
+    sitemap: "https://lolobuysheet.cc/sitemap-index.xml",
   };
 }
