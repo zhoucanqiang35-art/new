afa0d93a6ac755c81333204df6e21db0c3b1b901
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-26 14:00:33 +0800

    Trigger production sitemap refresh for lolobuy.id

--- lolobuy-id/app/sitemap.ts
+++ lolobuy-id/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { articles, categories, pages } from "./data";
 import { locales } from "./translations";
+
+// Canonical production origin for Google Search Console.
 export default function sitemap():MetadataRoute.Sitemap{
   const base="https://lolobuy.id",updated=new Date("2026-08-26");
   const paths=["",...Object.keys(pages),...categories.map(c=>`category/${c.slug}`),...articles.map(a=>`article/${a.slug}`)];
