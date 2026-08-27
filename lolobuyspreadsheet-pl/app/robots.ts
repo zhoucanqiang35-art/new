a9c8d2fe286c88cbd86966612402f4f6ed666891
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-08-27 18:37:21 +0800

    Point robots sitemap to lolobuyspreadsheet.pl

--- lolobuyspreadsheet-pl/app/robots.ts
+++ lolobuyspreadsheet-pl/app/robots.ts
@@ -8,6 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://lolobuyspreadsheet-pl.pages.dev/sitemap.xml",
+    sitemap: "https://lolobuyspreadsheet.pl/sitemap.xml",
   };
 }
