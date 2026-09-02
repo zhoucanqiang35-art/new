774a17bfd5a260f5d553b8f05de6ac9d60f76746
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-09-02 15:05:57 +0800

    Add XML sitemap response wrapper

---
+++ lolobuyspreadsheet-me/worker.js
@@ -0,0 +1,44 @@
+import siteWorker from "./site.js";
+
+const origin = "https://lolobuyspreadsheet.me";
+const routes = [
+  "/",
+  "/product-details",
+  "/how-lolobuy-works",
+  "/qc-guide",
+  "/shipping-guide",
+  "/buyer-checklist",
+  "/faq",
+  "/seo-articles",
+  "/seo-articles/how-to-use-lolobuy-spreadsheet",
+  "/seo-articles/lolobuy-shipcare-guide",
+];
+
+function sitemap() {
+  const body = routes.map((path) => {
+    const priority = path === "/" ? "1.0" : "0.8";
+    const frequency = path.startsWith("/seo-articles/") ? "monthly" : "weekly";
+    return "  <url><loc>" + origin + path + "</loc><lastmod>2026-09-02</lastmod><changefreq>" + frequency + "</changefreq><priority>" + priority + "</priority></url>";
+  }).join("\n");
+  return "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n" + body + "\n</urlset>";
+}
+
+export default {
+  async fetch(request, env, ctx) {
+    const { pathname } = new URL(request.url);
+    if (pathname === "/sitemap.xml") {
+      return new Response(sitemap(), {
+        headers: {
+          "content-type": "application/xml; charset=UTF-8",
+          "cache-control": "public, max-age=300",
+        },
+      });
+    }
+    if (pathname === "/robots.txt") {
+      return new Response("User-agent: *\nAllow: /\nSitemap: " + origin + "/sitemap.xml\n", {
+        headers: { "content-type": "text/plain; charset=UTF-8" },
+      });
+    }
+    return siteWorker.fetch(request, env, ctx);
+  },
+};
