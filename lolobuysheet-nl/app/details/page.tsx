18f764dd731fe4ce8e67a26de31f0407723154b9
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-09-08 15:28:08 +0800

    Deploy lolobuysheet-nl site

---
+++ lolobuysheet-nl/app/details/page.tsx
@@ -0,0 +1,2 @@
+import SiteExperience from "@/components/site-experience";
+export default function Details() { return <SiteExperience page="details" />; }
---
+++ lolobuysheet-nl/app/faq/page.tsx
@@ -0,0 +1,2 @@
+import SiteExperience from "@/components/site-experience";
+export default function Faq() { return <SiteExperience page="faq" />; }
