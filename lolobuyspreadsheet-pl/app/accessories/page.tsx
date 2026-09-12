a76f6ff4125f09b2c7c14e4da5a9e19e0de14f9f
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-09-12 10:11:52 +0800

    Add LoloBuy category SEO pages

---
+++ lolobuyspreadsheet-pl/app/accessories/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage, { categoryMetadata } from "@/components/category-page";
+export const metadata=categoryMetadata("accessories");
+export default function Page(){return <CategoryPage category="accessories"/>}
