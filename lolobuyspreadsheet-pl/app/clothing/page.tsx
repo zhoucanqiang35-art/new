7b288a6175c6e1a7735d89330b16b0c52a0839e0
zhoucanqiang35-art <zhoucanqiang35@gmail.com>
2026-09-12 10:11:47 +0800

    Add LoloBuy category SEO pages

---
+++ lolobuyspreadsheet-pl/app/clothing/page.tsx
@@ -0,0 +1,3 @@
+import CategoryPage, { categoryMetadata } from "@/components/category-page";
+export const metadata=categoryMetadata("clothing");
+export default function Page(){return <CategoryPage category="clothing"/>}
